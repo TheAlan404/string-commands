d17180b11a7800d210b083de5f63630f509257be
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-05-11 17:34:17 +0700

    improve index.js

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 // String Commands
 
-class CommandHandler {
+/**
+ * @typedef {Object} HandlerOptions
+ * @property {string} prefix The prefix for the handler.
+ */
+
+module.exports = class CommandHandler {
   /**
   * Command Handler Class
   * @constructor
-  * @param {object} [opts]
-  * @param {string} [opts.prefix]
+  * @param {HandlerOptions} opts
   */
   constructor(opts = {}) {
-    this.prefix = opts.prefix || "";
+    this.prefix = typeof opts.prefix === 'string' ? opts.prefix : "";
     this.commands = new Map();
   }
   
   /**
   * Sets the prefix for this handler
-  * @param {string} prefix
+  * @param {string} prefix The new prefix.
   */
   setPrefix(prefix) {
     this.prefix = typeof prefix == "string" ? prefix : "";
   }
 }
-
-module.exports = CommandHandler;

719aebffe8c2d8aa99bbb022d620a8f079d3c0f7
Dennis <43997085+TheAlan404@users.noreply.github.com>
2021-05-11 13:59:41 +0300

    stuff

--- index.js
+++ index.js
@@ -5,6 +5,43 @@
  * @property {string} prefix The prefix for the handler.
  */
 
+
+/**
+* A command argument. The first character of the string represents if it is required or not.
+* ':' means required and ';' means optional.
+* @typedef {string[]} CommandArgument
+* @example [":required1", ":required2", ";optional1"]
+*/
+
+/**
+* Represents a commandCallback. This function is run when the command gets run.
+* Do your command logic inside this function.
+* @callback commandCallback
+* @param {...any} customParams - If the handler class has a custom
+*                                commandCallbackArgs set, the callback's parameters will be set to that.
+*/
+
+class Command {
+  /**
+  * Represents a Command object
+  * You do not need to initialize this class to make CommandHandler#addCommand work,
+  * you can just use a normal object with the properties.
+  * @constructor
+  * @param {object} data
+  * @param {string} data.name - name of the command
+  * @param {(string[]|string)} [data.aliases] - aliases of this command (must be string[] if an object)
+  * @param {(CommandArgument[]|string[]) [data.usage] - shows the usage for this command
+  * @param {commandCallback} data.run
+  */
+  constructor(data) {
+    this.name = data.name || "ping";
+    this.aliases = data.aliases ? (Array.isArray(data.aliases) ? data.aliases : [data.aliases] ) : [];
+    this.usage = Array.isArray(data.usage) ? data.usage : [];
+    this.run = data.run;
+  }
+}
+
+
 module.exports = class CommandHandler {
   /**
   * Command Handler Class

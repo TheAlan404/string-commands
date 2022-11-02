3d793d098778bc947ff16c5671b2af9e3ab5624a
TheAlan404 <TheAlan404@users.noreply.github.com>
2022-11-02 11:37:21 +0000

    chore: prettier

--- plugins/discord.js
+++ plugins/discord.js
@@ -5,108 +5,115 @@ import { Client, channelMention, ChannelType } from "discord.js";
  * @type {Object<string, import("../src/ArgumentParser").UsageParser>}
  */
 const DiscordUsages = {
-    _mentionable: {
-        type: "text",
-        async parse(ctx) {
-            return ctx.arg.replace(/[<@#!&>]/g, "");
-        },
-    },
+	_mentionable: {
+		type: "text",
+		async parse(ctx) {
+			return ctx.arg.replace(/[<@#!&>]/g, "");
+		},
+	},
 
-    user: {
-        type: "_mentionable",
-        async parse(ctx) {
-            let user = ctx.context.client.users
-                .fetch(ctx.arg)
-                .catch(() => null);
+	user: {
+		type: "_mentionable",
+		async parse(ctx) {
+			let user = ctx.context.client.users
+				.fetch(ctx.arg)
+				.catch(() => null);
 
-            if (!user) {
-                return ctx.fail(`${ctx.opts.bot ? 'Bot' : 'User'} not found!`);
-            };
+			if (!user) {
+				return ctx.fail(`${ctx.opts.bot ? "Bot" : "User"} not found!`);
+			}
 
-            if (ctx.opts.bot && !user.bot)
-                return ctx.fail(`${ctx.style.bold(
-                    user.username
-                )} isn't a bot!`);
-            else if (!opts.bot && user.bot)
-                return ctx.fail(`${ctx.style.bold(user.username)} is a bot!`);
+			if (ctx.opts.bot && !user.bot)
+				return ctx.fail(
+					`${ctx.style.bold(user.username)} isn't a bot!`,
+				);
+			else if (!opts.bot && user.bot)
+				return ctx.fail(`${ctx.style.bold(user.username)} is a bot!`);
 
-            return { parsed: user };
-        },
-    },
+			return { parsed: user };
+		},
+	},
 
-    member: {
-        type: "user",
-        async parse(ctx) {
-            const { guild } = ctx.context.message;
-            const member = await guild.members
-                .fetch(ctx.arg.id)
-                .catch(() => null);
+	member: {
+		type: "user",
+		async parse(ctx) {
+			const { guild } = ctx.context.message;
+			const member = await guild.members
+				.fetch(ctx.arg.id)
+				.catch(() => null);
 
-            if (!member)
-                return ctx.fail(`${ctx.style.bold(
-                    ctx.arg.username
-                )} isn't on ${ctx.style.bold(guild.name)}!`);
+			if (!member)
+				return ctx.fail(
+					`${ctx.style.bold(
+						ctx.arg.username,
+					)} isn't on ${ctx.style.bold(guild.name)}!`,
+				);
 
-            return { parsed: member };
-        }
-    },
+			return { parsed: member };
+		},
+	},
 
-    role: {
-        type: "_mentionable",
-        async parse(ctx) {
-            const { guild } = ctx.context.message;
-            const role = await guild.roles.fetch(ctx.arg).catch(() => null);
+	role: {
+		type: "_mentionable",
+		async parse(ctx) {
+			const { guild } = ctx.context.message;
+			const role = await guild.roles.fetch(ctx.arg).catch(() => null);
 
-            if (!role)
-                return ctx.fail(`Role not found!`);
+			if (!role) return ctx.fail(`Role not found!`);
 
-            return { parsed: role };
-        },
-    },
+			return { parsed: role };
+		},
+	},
 
-    channel: {
-        type: "_mentionable",
-        async parse(ctx) {
-            const { guild } = ctx.context.message;
-            const channel = await guild.channels.fetch(ctx.arg).catch(() => null);
+	channel: {
+		type: "_mentionable",
+		async parse(ctx) {
+			const { guild } = ctx.context.message;
+			const channel = await guild.channels
+				.fetch(ctx.arg)
+				.catch(() => null);
 
-            if (!channel)
-                return ctx.fail(`Channel not found!`);
-            
-            if(ctx.opts.channelType !== undefined
-                && ctx.opts.channelType != channel.type) {
-                return ctx.fail(`${channelMention(channel.id)} isn't ${ctx.style.bold(ChannelType[ctx.opts.channelType])}`);
-            }
+			if (!channel) return ctx.fail(`Channel not found!`);
 
-            return { parsed: channel };
-        },
-    },
+			if (
+				ctx.opts.channelType !== undefined &&
+				ctx.opts.channelType != channel.type
+			) {
+				return ctx.fail(
+					`${channelMention(channel.id)} isn't ${ctx.style.bold(
+						ChannelType[ctx.opts.channelType],
+					)}`,
+				);
+			}
 
-    textChannel: {
-        type: "channel",
-        channelType: ChannelType.GuildText,
-    },
+			return { parsed: channel };
+		},
+	},
 
-    voiceChannel: {
-        type: "channel",
-        channelType: ChannelType.GuildVoice,
-    },
+	textChannel: {
+		type: "channel",
+		channelType: ChannelType.GuildText,
+	},
+
+	voiceChannel: {
+		type: "channel",
+		channelType: ChannelType.GuildVoice,
+	},
 };
 
-
 const DiscordChecks = {
-    requirePermissions(permissions) {},
+	requirePermissions(permissions) {},
 
-    async requireGuildOwner(client, msg, args) {
-        return msg.guild.ownerId == msg.author.id ?
-            {
-                pass: true,
-            } :
-            {
-                pass: false,
-                message: "You must be the owner of this guild",
-            };
-    },
+	async requireGuildOwner(client, msg, args) {
+		return msg.guild.ownerId == msg.author.id
+			? {
+					pass: true,
+			  }
+			: {
+					pass: false,
+					message: "You must be the owner of this guild",
+			  };
+	},
 };
 
 /**
@@ -114,41 +121,43 @@ const DiscordChecks = {
  */
 
 class DiscordCommandHandler extends CommandHandler {
-    /**
-     * 
-     * @param {Client} client The discord client
-     * @param {DiscordCommandHandlerOptions} opts - Options for the command handler
-     */
-    constructor(client, opts = {}) {
-        super(Object.assign({
-            argumentParser: {
-                styling: {
-                    arg: (x) => `\`${x}\``,
-                    bold: (x) => `**${x}**`,
-                },
-            },
-        }, opts));
-        this.client = client;
-        this.client.handler = this;
+	/**
+	 *
+	 * @param {Client} client The discord client
+	 * @param {DiscordCommandHandlerOptions} opts - Options for the command handler
+	 */
+	constructor(client, opts = {}) {
+		super(
+			Object.assign(
+				{
+					argumentParser: {
+						styling: {
+							arg: (x) => `\`${x}\``,
+							bold: (x) => `**${x}**`,
+						},
+					},
+				},
+				opts,
+			),
+		);
+		this.client = client;
+		this.client.handler = this;
 
-        for(let [id, usage] of Object.entries(DiscordUsages))
-            this.registerUsage(id, usage);
-    }
+		for (let [id, usage] of Object.entries(DiscordUsages))
+			this.registerUsage(id, usage);
+	}
 
-    buildArguments({ args, ctx }) {
-        return [ctx.client, ctx.message, args];
-    }
+	buildArguments({ args, ctx }) {
+		return [ctx.client, ctx.message, args];
+	}
 
-    // handles discord.js messages
-    handleMessage(msg) {
-        this.run(msg.content, {
-            message: msg,
-            client: this.client,
-        });
-    }
+	// handles discord.js messages
+	handleMessage(msg) {
+		this.run(msg.content, {
+			message: msg,
+			client: this.client,
+		});
+	}
 }
 
-export {
-    DiscordCommandHandler,
-    DiscordUsages,
-};
+export { DiscordCommandHandler, DiscordUsages };
